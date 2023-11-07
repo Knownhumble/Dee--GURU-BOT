6ad3d7654e810980d44d16d7d38f4b7e8eb3fe58
Knownhumble <116537443+Knownhumble@users.noreply.github.com>
2023-11-07 21:07:08 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['09075954615', 'me', true],
+  ['09075954615', 'Dee', true], 
   [''] 
 ] //Number of owners
 
